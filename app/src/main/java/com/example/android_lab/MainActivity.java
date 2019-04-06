ec8bbd409f2205f1afc2c5839ed839ae3398c7ec
LAPTOP-738OA63Q\Patryk <p-budnicki@wp.pl>
2019-04-06 11:27:56 +0200

    tworzenie listy wpisów

--- app/src/main/java/com/example/android_lab/MainActivity.java
+++ app/src/main/java/com/example/android_lab/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.android_lab;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
